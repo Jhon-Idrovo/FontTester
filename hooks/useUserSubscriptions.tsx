3d562c4ccf5256af787eabc1919f1fba1f0c21c6
Jhon Idrovo <jhonidrovo3@gmail.com>
2021-08-27 13:56:56 -0500

    d

--- hooks/useUserSubscriptions.tsx
+++ hooks/useUserSubscriptions.tsx
@@ -2,16 +2,22 @@ import { AxiosError } from "axios";
 import { useQuery } from "react-query";
 import Stripe from "stripe";
 import axiosInstance from "../lib/axios";
+import useUser from "./useUser";
 
 const useUserSubscriptions = () => {
+  const { user } = useUser();
   const {
     data: subscriptions,
     isLoading,
     isFetching,
     error,
-  } = useQuery("subscription", async () => {
-    return axiosInstance.get("/subscriptions").then((res) => res.data.data);
-  });
+  } = useQuery(
+    "subscription",
+    async () => {
+      return axiosInstance.get("/subscriptions").then((res) => res.data.data);
+    },
+    { enabled: user.role === "User" }
+  );
   return {
     subscriptions,
     isLoadingSubscriptions: isLoading || isFetching,
