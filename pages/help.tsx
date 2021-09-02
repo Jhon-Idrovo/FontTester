14b5353725e16fad2ccfb374ade5d8b253f1519e
Jhon Idrovo <jhonidrovo3@gmail.com>
2021-09-02 17:14:30 -0500

    help updated

--- pages/help.tsx
+++ pages/help.tsx
@@ -1,8 +1,29 @@
+import Link from "next/link";
+
 function Help() {
   return (
-    <div className="aux-message-container">
-      If you have any problems or recomendations please write an email to
-      font.tester.app@gmail.com. We'll read it ASAP.
+    <div className="">
+      <h1 className="t1 text-txt-base w-min mx-auto whitespace-nowrap mt-4">
+        Contact Information
+      </h1>
+      <p className="card-container_no-margin max-w-lg mx-auto table">
+        If you have any problem, write to the email below. Please send all the
+        information that could be helpful, such as email and username.
+      </p>
+      <Link href="mailto:font.tester.solve@gmail.com">
+        <a className="t2 text-txt-base w-min mx-auto table hover:text-primary">
+          font.tester.solve@gmail.com
+        </a>
+      </Link>
+      <p className="card-container_no-margin max-w-lg mx-auto table mt-12">
+        If you have any sugestion, send it to the email below. Please send your
+        username for a sending you a retribution.
+      </p>
+      <Link href="mailto:font.tester.app@gmail.com">
+        <a className="t2 text-txt-base w-min mx-auto table hover:text-primary">
+          font.tester.app@gmail.com
+        </a>
+      </Link>
     </div>
   );
 }
---
+++ pages/password-change.tsx
@@ -0,0 +1,22 @@
+function PasswordChange() {
+  return (
+    <div>
+      <form action="" className="flex flex-col">
+        <input
+          type="password"
+          name=""
+          id="password"
+          className="bg-secondary-input"
+        />
+        <input
+          type="password"
+          name=""
+          id="password-conf"
+          className="bg-secondary-input"
+        />
+      </form>
+    </div>
+  );
+}
+
+export default PasswordChange;
