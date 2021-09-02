2390eb47d3752855f2983276f5fd791b6f9a3fe7
Jhon Idrovo <jhonidrovo3@gmail.com>
2021-09-02 18:55:11 -0500

    delete account working

--- components/SubscriptionNeeded.tsx
+++ components/SubscriptionNeeded.tsx
@@ -4,9 +4,11 @@ function SubscriptionNeeded() {
   return (
     <div className="container-full">
       <div className="container-full-inner">
-        <p>You need a subscription to acces this </p>
+        <p>You need an account to acces this </p>
         <Link href="/signup">
-          <a className="btn px-2 w-min mx-auto mt-2">Join</a>
+          <a className="btn px-2 w-min mx-auto mt-2 whitespace-nowrap">
+            Join For Free
+          </a>
         </Link>{" "}
       </div>
     </div>
